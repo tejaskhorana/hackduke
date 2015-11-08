9ba2ec29d353a4c8f7384605790ddcf6f2fcb5a9
Tejas Khorana <tejaskhorana@gmail.com>
2015-11-07 22:11:59 -0500

    moving people and eatable foods

--- Person.js
+++ Person.js
@@ -1,9 +1,17 @@
-function Person(x, y) {
+function Person(x, y, xBound, yBound, xVelocity, yVelocity) {
     this.x = x;
     this.y = y;
+    this.lowerXBound = x - xBound;
+    this.upperXBound = x + xBound;
+    this.lowerYBound = y - yBound;
+    this.upperYBound = y + yBound;
+    this.xVelocity = xVelocity;
+    this.yVelocity = yVelocity;
 
     this.setImage = function(game, imgName) {
     	this.img = game.add.sprite(this.x, this.y, imgName);
+    	this.imgName = imgName;
     }
+
 }
 Person.prototype.type = "person";
--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var game = new Phaser.Game(1200, 800, Phaser.AUTO, 'game_div');
 
 var keyUp, keyDown, keyLeft, keyRight;
 
-var player, person1;
+var player;
+
+var personArray;
+var person1, person2, person3, person4;
 
 var foodArray;
 var food1, food2, food3, food4;
@@ -13,33 +16,51 @@ var labelScore, labelTimer;
 
 var main_state = {
     preload: function () {
-        game.load.image('person', 'assets/Person.png');
+
+        game.load.image('person1', 'assets/person1.png');
+        game.load.image('person2', 'assets/person2.png');
+        game.load.image('person3', 'assets/person3.png');
+        game.load.image('person4', 'assets/person4.png');
+
+
+
         game.load.image('player', 'assets/Player.png');
 
-        game.load.image('food', 'assets/food1.png');
-        game.load.image('food', 'assets/food2.png');
-        game.load.image('food', 'assets/food3.png');
-        game.load.image('food', 'assets/food4.png');
+        game.load.image('food1', 'assets/food1.png');
+        game.load.image('food2', 'assets/food2.png');
+        game.load.image('food3', 'assets/food3.png');
+        game.load.image('food4', 'assets/food4.png');
 
         gameStatus = 'win';
     },
 
     create: function () {
-        person1 = new Person(150, 150);
-        person1.setImage(game, 'person');
+
+        //initialize ALL persons in designated spots and assign to an array
+        person1 = new Person(150, 150, 60, 60, (Math.random()-.5)*20, (Math.random()-.5)*20);
+        person1.setImage(game, 'person1');
+        person2 = new Person(350, 250, 70, 70, (Math.random()-.5)*50, (Math.random()-.5)*50);
+        person2.setImage(game, 'person2');
+        person3 = new Person(550, 350, 100, 100, (Math.random()-.5)*50, (Math.random()-.5)*100);
+        person3.setImage(game, 'person3');
+        person4 = new Person(330, 420, 80, 80, (Math.random()-.5)*50, (Math.random()-.5)*90);
+        person4.setImage(game, 'person4');
+        personArray = [person1, person2, person3, person4];
+
+
         player = new Player(125, 250);
         player.setImage(game, 'player');
 
 
         //initialize ALL food in designated spots and assign to an array
         food1 = new Food(155, 165);
-        food1.setImage(game, 'food');
+        food1.setImage(game, 'food1');
         food2 = new Food(255, 165);
-        food2.setImage(game, 'food');
+        food2.setImage(game, 'food2');
         food3 = new Food(355, 165);
-        food3.setImage(game, 'food');
+        food3.setImage(game, 'food3');
         food4 = new Food(355, 65);
-        food4.setImage(game, 'food');
+        food4.setImage(game, 'food4');
         foodArray = [food1, food2, food3, food4]; 
 
         score = 0;
@@ -57,6 +78,21 @@ var main_state = {
 
     update: function () {
 
+        //handles movement of all Persons on map
+        for(var i = 0; i < personArray.length ;i++) {
+            if(personArray[i].x < personArray[i].lowerXBound || personArray[i].x > personArray[i].upperXBound) {
+                personArray[i].xVelocity = personArray[i].xVelocity*-1;
+            }
+            if(personArray[i].y < personArray[i].lowerYBound || personArray[i].y > personArray[i].upperYBound) {
+                personArray[i].yVelocity = personArray[i].yVelocity*-1;
+            }
+            console.log(personArray[i].xVelocity);
+            personArray[i].x += 2*personArray[i].xVelocity;
+            personArray[i].y += 2*personArray[i].yVelocity;
+            this.game.world.remove(personArray[i].img);
+            personArray[i].setImage(game, personArray[i].imgName.toString());
+        }
+
         //iterates through all foods and deletes removes all images and foods that intersect with player
         for(var i = 0; i < foodArray.length; i++) {
             if(collides(player, foodArray[i])) {
@@ -69,7 +105,7 @@ var main_state = {
             }
         }
 
-        
+        //dealing with timer        
         if(timer > 0) {
             timer -= 1/60;
             this.game.world.remove(labelTimer);
