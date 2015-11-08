5f7c793f0977e8dc80af197f8b49e6abfda1b7e3
Tejas Khorana <tejaskhorana@gmail.com>
2015-11-07 21:02:51 -0500

    groups of food stuffs

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var game = new Phaser.Game(1200, 800, Phaser.AUTO, 'game_div');
 
 var keyUp, keyDown, keyLeft, keyRight;
 
-var player, person1, food1;
+var player, person1;
+
+var foodArray;
+var food1, food2, food3, food4;
 
 var score, timer, gameStatus;
 
@@ -12,7 +15,12 @@ var main_state = {
     preload: function () {
         game.load.image('person', 'assets/Person.png');
         game.load.image('player', 'assets/Player.png');
-        game.load.image('food', 'assets/food.png');
+
+        game.load.image('food', 'assets/food1.png');
+        game.load.image('food', 'assets/food2.png');
+        game.load.image('food', 'assets/food3.png');
+        game.load.image('food', 'assets/food4.png');
+
         gameStatus = 'win';
     },
 
@@ -21,8 +29,18 @@ var main_state = {
         person1.setImage(game, 'person');
         player = new Player(125, 250);
         player.setImage(game, 'player');
+
+
+        //initialize ALL food in designated spots and assign to an array
         food1 = new Food(155, 165);
         food1.setImage(game, 'food');
+        food2 = new Food(255, 165);
+        food2.setImage(game, 'food');
+        food3 = new Food(355, 165);
+        food3.setImage(game, 'food');
+        food4 = new Food(355, 65);
+        food4.setImage(game, 'food');
+        foodArray = [food1, food2, food3, food4]; 
 
         score = 0;
         timer = 10.0;
@@ -34,24 +52,24 @@ var main_state = {
         keyLeft = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         keyRight = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
-        //key3 = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        //key3.onDown.add(addPineapple, this);
 
     },
 
     update: function () {
 
-
-        if(collides(player, food1)) {
-            this.game.world.remove(food1.img);
-            food1 = null;
-            score = score + 5;
-            this.game.world.remove(labelScore);
-            labelScore = game.add.text(20, 10, "Score: " + score, { font: "30px Arial", fill: "#ffffff" });         
-
-            //remove reference to food1 in the future
+        //iterates through all foods and deletes removes all images and foods that intersect with player
+        for(var i = 0; i < foodArray.length; i++) {
+            if(collides(player, foodArray[i])) {
+                this.game.world.remove(foodArray[i].img);
+                foodArray.splice(i, 1);
+                score = score + 5;
+                this.game.world.remove(labelScore);
+                labelScore = game.add.text(20, 10, "Score: " + score, { font: "30px Arial", fill: "#ffffff" });         
+                i = i - 1;
+            }
         }
 
+        
         if(timer > 0) {
             timer -= 1/60;
             this.game.world.remove(labelTimer);
