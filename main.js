610c16c63c0ee5484d88568b1d8e063d05b89146
Joshua Vorick <josh.vorick@gmail.com>
2015-11-08 03:35:32 +0000

    Camera and wall collision

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var game = new Phaser.Game(1200, 800, Phaser.AUTO, 'game_div');
+var game = new Phaser.Game(800, 800, Phaser.AUTO, 'game_div');
+
+var bmd;
 
 var keyUp, keyDown, keyLeft, keyRight;
 
@@ -8,28 +10,37 @@ var main_state = {
     preload: function () {
         game.load.image('person', 'assets/Person.png');
         game.load.image('player', 'assets/Player.png');
+        game.load.image('walls', 'assets/level1Walls.bmp');
+        game.load.image('background', 'assets/background.png');
         this.gameStatus = 'win';
     },
 
     create: function () {
+        bmd = game.make.bitmapData(4000, 4000);
+        bmd.draw(game.cache.getImage('walls'), 0, 0);
+        bmd.update();
+
+        game.add.tileSprite(0, 0, 4000, 4000, 'background');
+
+        game.world.setBounds(0, 0, 4000, 4000);
+
+        game.physics.startSystem(Phaser.Physics.P2JS);
+
         person1 = new Person(150, 150);
         person1.setImage(game, 'person');
-        player = new Player(125, 250);
+        player = new Player(1600, 1600);
         player.setImage(game, 'player');
+
+        game.physics.p2.enable(player.img);
+
+        cursors = game.input.keyboard.createCursorKeys();
+
+        game.camera.follow(player.img);
 
         this.score = 0;
         this.timer = 10.0;
         this.labelScore = game.add.text(20, 10, "Score: " + this.score.toString(), { font: "30px Arial", fill: "#ffffff" });         
         this.labelScore = game.add.text(20, 40, "Timer: " + this.timer.toString(), { font: "30px Arial", fill: "#ffffff" }); 
-
-        keyUp = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        keyDown = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-        keyLeft = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        keyRight = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-
-        //key3 = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        //key3.onDown.add(addPineapple, this);
-
     },
 
     update: function () {
@@ -45,20 +56,27 @@ var main_state = {
 
         } 
 
-        if (keyUp.isDown) {
-            player.moveUp();
+        player.img.body.setZeroVelocity();
+
+        if (cursors.up.isDown) {
+            player.moveUp(bmd);
         }
-        if (keyDown.isDown) {
-            player.moveDown();
+        else if (cursors.down.isDown) {
+            player.moveDown(bmd);
         }
 
-        if (keyLeft.isDown) {
-            player.moveLeft();
+        if (cursors.left.isDown) {
+            player.moveLeft(bmd);
         }
-        if (keyRight.isDown) {
-            player.moveRight();
+        else if (cursors.right.isDown) {
+            player.moveRight(bmd);
         }
-    }
+    }//,
+
+    //render: function() {
+        //game.debug.cameraInfo(game.camera, 32, 32);
+        //game.debug.spriteCoords(player, 32, 500);
+    //}
 }
 
 game.state.add('main', main_state);
