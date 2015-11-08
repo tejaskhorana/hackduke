4b33e72b538efcb268e38ffd3386c8f8b8fb6695
Tejas Khorana <tejaskhorana@gmail.com>
2015-11-08 05:48:16 -0500

    stuffs

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ var player;
 var level, maxLevels;
 
 var personArray;
-var person1, person2, person3;
+var person1, person2, person3, person4;
 
 var foodArray;
-var food1, food2, food3, food4;
+var food1, food2, food3, food4, food5, food6, food7, food8;
 
 var foodRequired, foodCollected, timer, gameStatus;
 
@@ -39,6 +39,7 @@ var main_state = {
         game.load.image('food2', 'assets/food2.png');
         game.load.image('food3', 'assets/food3.png');
         game.load.image('food4', 'assets/food4.png');
+        game.load.image('food4', 'assets/food5.png');
 
         game.load.image('background', 'assets/background.png');
         game.load.image('background2', 'assets/background2.png');
@@ -202,7 +203,7 @@ function nextPokemonText() {
 }
 
 function initializeAllPokemonText() {
-    pokemonTextOne = "Welcome to the top level of the Tower! In this\n world, Dantie is a rich businessman and founder\n of a massive software company called\n Macrosoft. He has several key executives\n working underneath him and therefore only has\n to work a few hours a day. He has\n plenty of money and time, free to enjoy the luxu-\nries of the Tower. His house is nice and spacious,\n the epitome of modern decadence.\n Today, Dante has to eat three meals, put in a\n few hours of work at the Macrosoft building next\n door, and just enjoy the town before he has to\n head back home to catch some sleep.\n Good luck!.... HIT ENTER";
+    pokemonTextOne = "Welcome to the top level of the Tower! In this\n world, Dante is a rich businessman and founder\n of a massive software company called\n Macrosoft. He has several key executives\n working underneath him and therefore only has\n to work a few hours a day. He has\n plenty of money and time, free to enjoy the luxu-\nries of the Tower. His house is nice and spacious,\n the epitome of modern decadence.\n Today, Dante has to eat three meals, put in a\n few hours of work at the Macrosoft building next\n door, and just enjoy the town before he has to\n head back home to catch some sleep.\n Good luck!.... HIT ENTER";
     pokemonTextTwo = "Welcome to the second level of the Tower! In\n this world, Dante is a branch manager of\n Moontrust, the only bank in the Tower. he has\n a couple of employees working for him, but he\n still has to work one shift a day. he is fairly well\n off, and he and his wife enjoy their life in the\n Tower. his house is pretty nice, though they\n have to share the other half with another resi-\n dent. \n Today, Dante just has to eat three meals, \nput in some hours of work at the bank, and \nmaybe have a short walk around town before \nhe heads back home to go to bed. Good luck!\n.... HIT ENTER";
     pokemonTextThree = "Welcome to the third level of the Tower. In\n this world, Dante is a clerk at the grocery\n store, Jingles. He works two shifts and is\n under a fair amount of stress every day.\n He can barely support his family of\n three and he barely has time to eat,\n let alone enjoy himself.\n His family share the house with three other\n families, and it is often dank and cramped.\n Today, Dante has to eat three meals, put in\n several hours of work at the store, and make\n sure he gets everything done in time before\n he needs to sleep. Good luck.... HIT ENTER";
     pokemonTextFour = "Welcome to the bottom level of the Tower. In this\n world, Dante is a poor waiter at Outfront, a\n restaurant in the corner of town. he alone has to\n support his family of four, and therefore he works\n the entire day at the restaurant. he does not even\n have enough money to afford to eat there; he has to\n get his food at the store after work before it closes.\n Dante is often stressed and tired, but there is nothing\n he can do about it. his family shares their meager\n quarters with six other families in their building.\n Today, Dante just has to eat three meals, work at\n the restaurant for most of the day, go to the store\n before it closes, and make it back home to get\n enough sleep for the next day.\n Good luck! .... HIT ENTER!";
@@ -316,34 +317,31 @@ function collides(a, b)
 */
 function populateMap() {
 
-    if(level == 1) {
         bmd = game.make.bitmapData(4000, 4000);
         bmd.draw(game.cache.getImage('walls1'), 0, 0);
         bmd.update();
 
         game.add.tileSprite(0, 0, 4000, 4000, 'level1bg');
 
-        foodRequired = 20;
-        foodCollected = 0;
-        labelFoodRequired = game.add.text(20, 10, "Food Collected: " + foodCollected + "/" + foodRequired, { font: "30px Arial", fill: "#ffffff" });         
-
-        timer = 10.0;
-        labelTimer = game.add.text(20, 40, "Timer: " + timer.toString(), { font: "30px Arial", fill: "#ffffff" }); 
-
         //initialize ALL persons in designated spots and assign to an array
-        person1 = new Person(450, 450, 100, 40, (Math.random()-.5)*4, (Math.random()-.5)*5);
+        person1 = new Person(1655, 2700, 0,300, 0, 2);
         person1.setImage(game, 'person1');
-        person2 = new Person(350, 250, 70, 70, (Math.random()-.5)*5, (Math.random()-.5)*8);
+        person2 = new Person(2600, 1350, 250, 250, 1, 1);
         person2.setImage(game, 'person2');
-        person3 = new Person(550, 350, 100, 100, (Math.random()-.5)*8, (Math.random()-.5)*5);
+        person3 = new Person(1200, 1600, 300, 0, 2, 0);
         person3.setImage(game, 'person3');
+        person4 = new Person(1570, 900, 0, 400, 0, 3);
+        person4.setImage(game, 'person1');        
+        person5 = new Person(3450, 1630, 300, 0, 2, 0);
+        person5.setImage(game, 'person2');
 
-        personArray = [person1, person2, person3];
+        personArray = [person1, person2, person3, person4, person5];
 
-        player = new Player(125, 250);
+        player = new Player(900, 3800);
         player.setImage(game, 'player');
         game.physics.p2.enable(player.img);
         game.camera.follow(player.img);
+
 
         //initialize ALL food in designated spots and assign to an array
         food1 = new Food(155, 165);
@@ -354,124 +352,58 @@ function populateMap() {
         food3.setImage(game, 'food3');
         food4 = new Food(355, 65);
         food4.setImage(game, 'food4');
-        foodArray = [food1, food2, food3, food4];         
+        food5 = new Food(155, 165);
+        food5.setImage(game, 'food5');
+        food6 = new Food(255, 165);
+        food6.setImage(game, 'food1');
+        food7 = new Food(355, 165);
+        food7.setImage(game, 'food2');
+        food8 = new Food(355, 65);
+        food8.setImage(game, 'food3');
+        food9 = new Food(355, 165);
+        food9.setImage(game, 'food4');
+        food10 = new Food(355, 65);
+        food10.setImage(game, 'food5');
+        foodArray = [food1, food2, food3, food4, food5, food6, food7, food8, food9, food10]; 
+    if(level == 1) {
+
+        
+        foodRequired = 20;
+        foodCollected = 0;
+        labelFoodRequired = game.add.text(20, 10, "Food Collected: " + foodCollected + "/" + foodRequired, { font: "30px Arial", fill: "#ffffff" });         
+
+        timer = 50.0;
+        labelTimer = game.add.text(20, 40, "Timer: " + timer.toString(), { font: "30px Arial", fill: "#ffffff" }); 
+
+        foodArray = [food1, food2, food3, food4,food5,food6,food7];         
     } else if (level == 2) {
-        bmd = game.make.bitmapData(4000, 4000);
-        bmd.draw(game.cache.getImage('walls2'), 0, 0);
-        bmd.update();
-
-        game.add.tileSprite(0, 0, 4000, 4000, 'level2bg');
-
-        foodRequired = 20;
+        foodRequired = 25;
         foodCollected = 0;
         labelFoodRequired = game.add.text(20, 10, "Food Collected: " + foodCollected + "/" + foodRequired, { font: "30px Arial", fill: "#ffffff" });         
 
-        timer = 10.0;
+        timer = 45.0;
         labelTimer = game.add.text(20, 40, "Timer: " + timer.toString(), { font: "30px Arial", fill: "#ffffff" }); 
 
-        //initialize ALL persons in designated spots and assign to an array
-        person1 = new Person(450, 450, 100, 40, (Math.random()-.5)*4, (Math.random()-.5)*5);
-        person1.setImage(game, 'person1');
-        person2 = new Person(350, 250, 70, 70, (Math.random()-.5)*5, (Math.random()-.5)*8);
-        person2.setImage(game, 'person2');
-        person3 = new Person(550, 350, 100, 100, (Math.random()-.5)*8, (Math.random()-.5)*5);
-        person3.setImage(game, 'person3');
-
-        personArray = [person1, person2, person3];
-
-        player = new Player(125, 250);
-        player.setImage(game, 'player');
-        game.physics.p2.enable(player.img);
-        game.camera.follow(player.img);
-
-        //initialize ALL food in designated spots and assign to an array
-        food1 = new Food(155, 165);
-        food1.setImage(game, 'food1');
-        food2 = new Food(255, 165);
-        food2.setImage(game, 'food2');
-        food3 = new Food(355, 165);
-        food3.setImage(game, 'food3');
-        food4 = new Food(355, 65);
-        food4.setImage(game, 'food4');
-        foodArray = [food1, food2, food3, food4];  
+        foodArray = [food1, food2, food3, food4,food5,food6,food7];  
     } else if (level == 3) {
-        bmd = game.make.bitmapData(4000, 4000);
-        bmd.draw(game.cache.getImage('walls3'), 0, 0);
-        bmd.update();
 
-        game.add.tileSprite(0, 0, 4000, 4000, 'level3bg');
-
-        foodRequired = 20;
+        foodRequired = 40;
         foodCollected = 0;
         labelFoodRequired = game.add.text(20, 10, "Food Collected: " + foodCollected + "/" + foodRequired, { font: "30px Arial", fill: "#ffffff" });         
 
-        timer = 10.0;
+        timer = 35.0;
         labelTimer = game.add.text(20, 40, "Timer: " + timer.toString(), { font: "30px Arial", fill: "#ffffff" }); 
 
-        //initialize ALL persons in designated spots and assign to an array
-        person1 = new Person(450, 450, 100, 40, (Math.random()-.5)*4, (Math.random()-.5)*5);
-        person1.setImage(game, 'person1');
-        person2 = new Person(350, 250, 70, 70, (Math.random()-.5)*5, (Math.random()-.5)*8);
-        person2.setImage(game, 'person2');
-        person3 = new Person(550, 350, 100, 100, (Math.random()-.5)*8, (Math.random()-.5)*5);
-        person3.setImage(game, 'person3');
-
-        personArray = [person1, person2, person3];
-
-        player = new Player(125, 250);
-        player.setImage(game, 'player');
-        game.physics.p2.enable(player.img);
-        game.camera.follow(player.img);
-
-        //initialize ALL food in designated spots and assign to an array
-        food1 = new Food(155, 165);
-        food1.setImage(game, 'food1');
-        food2 = new Food(255, 165);
-        food2.setImage(game, 'food2');
-        food3 = new Food(355, 165);
-        food3.setImage(game, 'food3');
-        food4 = new Food(355, 65);
-        food4.setImage(game, 'food4');
-        foodArray = [food1, food2, food3, food4];          
+         
     } else if (level == 4) {
-        bmd = game.make.bitmapData(4000, 4000);
-        bmd.draw(game.cache.getImage('walls4'), 0, 0);
-        bmd.update();
 
-        game.add.tileSprite(0, 0, 4000, 4000, 'level4bg');
 
-        foodRequired = 20;
+        foodRequired = 50;
         foodCollected = 0;
         labelFoodRequired = game.add.text(20, 10, "Food Collected: " + foodCollected + "/" + foodRequired, { font: "30px Arial", fill: "#ffffff" });         
 
-        timer = 10.0;
+        timer = 25.0;
         labelTimer = game.add.text(20, 40, "Timer: " + timer.toString(), { font: "30px Arial", fill: "#ffffff" }); 
-
-        //initialize ALL persons in designated spots and assign to an array
-        person1 = new Person(450, 450, 100, 40, (Math.random()-.5)*4, (Math.random()-.5)*5);
-        person1.setImage(game, 'person1');
-        person2 = new Person(350, 250, 70, 70, (Math.random()-.5)*5, (Math.random()-.5)*8);
-        person2.setImage(game, 'person2');
-        person3 = new Person(550, 350, 100, 100, (Math.random()-.5)*8, (Math.random()-.5)*5);
-        person3.setImage(game, 'person3');
-
-        personArray = [person1, person2, person3];
-
-        player = new Player(125, 250);
-        player.setImage(game, 'player');
-        game.physics.p2.enable(player.img);
-        game.camera.follow(player.img);
-
-        //initialize ALL food in designated spots and assign to an array
-        food1 = new Food(155, 165);
-        food1.setImage(game, 'food1');
-        food2 = new Food(255, 165);
-        food2.setImage(game, 'food2');
-        food3 = new Food(355, 165);
-        food3.setImage(game, 'food3');
-        food4 = new Food(355, 65);
-        food4.setImage(game, 'food4');
-        foodArray = [food1, food2, food3, food4];          
 
     } 
 
